9c2d2d2258588f43919cecf4ff7f3c92409e9de4
Carlie-Dev <ellepheonixgames@gmail.com>
2024-03-01 08:32:13 -0600

    negative indexs and getting the middle of a URL

---
+++ Module7Practice.py
@@ -0,0 +1,9 @@
+webUrl = 'https://my.otc.edu'
+
+#Get the middle part of a url
+#the first number is inclusive. The first number is nonincludive.
+print(webUrl[8:14])
+
+#negative indexes allow you to count from the end of the string aswell
+print(webUrl[8:-4])
+
